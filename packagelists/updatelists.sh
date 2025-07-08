6c163073f644f8d5ab1532e43d24d0be7a9e4eff
rylan <rylanmoseley@gmail.com>
2025-07-07 23:54:21 -0400

    automatically update package lists

--- packagelists/updatelists.sh
+++ packagelists/updatelists.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-cd ~/.config/packagelists
+cd /home/rylan/.config/packagelists
 
 pacman -Q > allpackages.txt
 pacman -Qen > pacmanpackages.txt
 pacman -Qm > aurpackages.txt
 
+su rylan
 git add *
 git commit -m "automatically update package lists"
 git push
